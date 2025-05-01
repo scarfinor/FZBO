f2a36b494f8a61093d31f273b6713bd891c89fba
scarfinor <scarfinor@gmail.com>
2025-04-30 22:01:27 -0500

    CommandLineRunner update (USER, MODERATOR, ADMIN)

--- 2025/src/main/java/com/FZBO/FZBOApplication.java
+++ 2025/src/main/java/com/FZBO/FZBOApplication.java
@@ -18,14 +18,14 @@ public class FZBOApplication {
 	@Bean
 	CommandLineRunner initRoles(RoleRepository roleRepository) {
 		return args -> {
-			if (roleRepository.findByName(ERole.ROLE_USER).isEmpty()) {
-				roleRepository.save(new Role(ERole.ROLE_USER));
+			if (roleRepository.findByName(ERole.USER).isEmpty()) {
+				roleRepository.save(new Role(ERole.USER));
 			}
-			if (roleRepository.findByName(ERole.ROLE_MODERATOR).isEmpty()) {
-				roleRepository.save(new Role(ERole.ROLE_MODERATOR));
+			if (roleRepository.findByName(ERole.MODERATOR).isEmpty()) {
+				roleRepository.save(new Role(ERole.MODERATOR));
 			}
-			if (roleRepository.findByName(ERole.ROLE_ADMIN).isEmpty()) {
-				roleRepository.save(new Role(ERole.ROLE_ADMIN));
+			if (roleRepository.findByName(ERole.ADMIN).isEmpty()) {
+				roleRepository.save(new Role(ERole.ADMIN));
 			}
 		};
 	}
