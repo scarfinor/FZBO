5b65bb9a365901eeb8274a6866093f1ce4aa8a79
scarfinor <scarfinor@gmail.com>
2025-04-30 22:10:40 -0500

    Role.java (id and name) RoleRepository.java (Role, Integer/findByName())

--- 2025/src/main/java/com/FZBO/models/Role.java
+++ 2025/src/main/java/com/FZBO/models/Role.java
@@ -1,4 +1,42 @@
 package com.FZBO.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    @NotNull(message = "Role must not be null")
+    @NotBlank(message = "Role must not be blank")
+    private ERole name;
+
+    public Role() {}
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
 }
--- 2025/src/main/java/com/FZBO/repos/RoleRepository.java
+++ 2025/src/main/java/com/FZBO/repos/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.FZBO.repos;
 
-public interface RoleRepository {
+import com.FZBO.models.ERole;
+import com.FZBO.models.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+    Optional<Role> findByName(ERole name);
 }
