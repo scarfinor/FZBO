893bfd819354511e821e6c8819a2788eba6d5f77
scarfinor <scarfinor@gmail.com>
2025-04-30 22:47:22 -0500

    SocialMedia.java (id, platform, url, user)

--- 2025/src/main/java/com/FZBO/models/SocialMedia.java
+++ 2025/src/main/java/com/FZBO/models/SocialMedia.java
@@ -1,4 +1,55 @@
 package com.FZBO.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "social_medias")
 public class SocialMedia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String platform;
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public SocialMedia() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
