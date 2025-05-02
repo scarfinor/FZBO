044f4353379350bbb29993839ade697b4c78f291
scarfinor <scarfinor@gmail.com>
2025-05-01 21:02:53 -0500

    AuthEntryPoint.java (logger)

--- BackEnd/2025/src/main/java/com/FZBO/security/jwts/AuthEntryPoint.java
+++ BackEnd/2025/src/main/java/com/FZBO/security/jwts/AuthEntryPoint.java
@@ -1,4 +1,19 @@
 package com.FZBO.security.jwts;
 
-public class AuthEntryPoint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class AuthEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws java.io.IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
 }
