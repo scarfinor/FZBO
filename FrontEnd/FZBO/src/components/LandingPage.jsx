0d56bc0d1859459d4d178e3f3d8c7cb00305bbf4
scarfinor <scarfinor@gmail.com>
2025-05-04 20:41:59 -0500

    rendered Sign In and Sign Up components on LandingPage.jsx

--- FrontEnd/FZBO/src/components/LandingPage.jsx
+++ FrontEnd/FZBO/src/components/LandingPage.jsx
@@ -1,8 +1,28 @@
+import SignIn from './SignIn.jsx';
+import SignUp from "./SignUp.jsx";
+
 export default function LandingPage() {
     return (
         <div>
+            <div className="landingPage">
             <h1>Welcome to the Landing Page</h1>
             <img src="/assets/fzbo_compressed.jpg" alt="FZBO logo" />
+            </div>
+
+            <div className="signIn">
+            <p> Have a FBZO Account allready?</p>
+            <div>
+                <SignIn />
+            </div>
+            </div>
+
+            <div className="signUp">
+            <p> Don't have an FBZO Account?</p>
+            <div>
+                <SignUp />
+            </div>
+            </div>
+
         </div>
     );
 }
